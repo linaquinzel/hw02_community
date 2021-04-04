3fbe9d3472c30bdc972d74e46cede91fbab63931
Liya Yazdanova <liya.yazdanova@mail.ru>
2021-04-04 18:44:08 +0500

    homework1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,19 +3,21 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey('Group', on_delete=models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, 
+                               related_name="posts")
+    group = models.ForeignKey('Group', 
+                              on_delete=models.CASCADE, blank=True, null=True)
 
-    
+
 class Group(models.Model):
     group_id = models.CharField(primary_key=True, max_length=1000)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=80, unique=True)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.title
-    
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,16 +2,13 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
+
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
     return render(request, "index.html", {"posts": latest})
 
-def group_posts(request, slug):
-    # функция get_object_or_404 получает по заданным критериям объект из базы данных 
-    # или возвращает сообщение об ошибке, если объект не найден
-    group = get_object_or_404(Group, slug=slug)
 
-    # Метод .filter позволяет ограничить поиск по критериям. Это аналог добавления
-    # условия WHERE group_id = {group_id}
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
     return render(request, "group.html", {"groups": group, "posts": posts})
