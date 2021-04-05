f3d77c6e3b06fa4613f4e48a25db0dab76ec1784
Liya Yazdanova <liya.yazdanova@mail.ru>
2021-04-05 18:16:01 +0500

    meta

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,14 +6,20 @@ User = get_user_model()
 
 class Post(models.Model):
     text = models.TextField()
+
     pub_date = models.DateTimeField(verbose_name="date published",
                                     auto_now_add=True)
     author = models.ForeignKey(User, verbose_name="author",
                                on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey("Group", blank=True, null=True)
+    group = models.ForeignKey("Group", on_delete=models.SET_NULL, blank=True, null=True)
+    
+    class Meta:
+        verbose_name_plural = "Сообщества"
+        ordering = ['-pub_date']
 
 
 class Group(models.Model):
+    group_id = models.CharField(primary_key=True, max_length=1000)
     title = models.CharField(verbose_name="title", max_length=200)
     slug = models.SlugField(unique=True, verbose_name="slug")
     description = models.TextField(verbose_name="description")
