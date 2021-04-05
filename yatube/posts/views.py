4ea48e007be27714b511be99d8a3ee49240d2e54
Liya Yazdanova <liya.yazdanova@mail.ru>
2021-04-05 11:28:12 +0500

    withoutexcess

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -11,6 +11,4 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    description = Group.description
-    return render(request, "group.html", {"groups": group, "posts": posts,
-                                          "description": description})
+    return render(request, "group.html", {"groups": group, "posts": posts})
