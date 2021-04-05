cbac674e19ec02009885ced70d805229f566acd5
Liya Yazdanova <liya.yazdanova@mail.ru>
2021-04-05 10:59:38 +0500

    best

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -11,4 +11,5 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    return render(request, "group.html", {"groups": group, "posts": posts})
+    description = Group.description
+    return render(request, "group.html", {"groups": group, "posts": posts, "description": description})
