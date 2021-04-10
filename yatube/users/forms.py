4869c2890c2d51858ad60cb43509df374bd58032
Liya Yazdanova <liya.yazdanova@mail.ru>
2021-04-10 18:56:03 +0500

    finale

--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    name = forms.CharField(label="Введите имя")
+    sender = forms.EmailField(label="Email для ответа")
+    subject = forms.CharField(label="Тема сообщения", initial='Письмо администратору', max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    cc_myself = forms.BooleanField(label="Отправить себе копию", required=False)
+    
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'subject', 'body') 
 
 
 User = get_user_model()
@@ -11,11 +23,3 @@ class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("first_name", "last_name", "username", "email")
-
-
-class ContactForm(forms.Form):
-        name = forms.CharField(label="Введите имя")
-        sender = forms.EmailField(label="Email для ответа")
-        subject = forms.CharField(label="Тема сообщения", initial='Письмо администратору', max_length=100)
-        message = forms.CharField(widget=forms.Textarea)
-        cc_myself = forms.BooleanField(label="Отправить себе копию", required=False) 
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -9,3 +9,10 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("signup") #  где signup — это параметр "name" в path()
     template_name = "signup.html"
+
+
+def user_contact(request):
+    contact = Contact.objects.get(pk=3)
+    form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
+    
